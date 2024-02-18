894dd7f4e71675a5506139bcfe0bb77f57e0820f
katie <katie@zwinzler.com>
2024-02-18 17:43:41 -0500

    inital commit of my tests

---
+++ main.py
@@ -0,0 +1,51 @@
+import unittest
+import argparse
+import sys
+import send
+
+#These aren't working right now, need to figure this out!
+def parse_args():
+    parser = argparse.ArgumentParser(description='Unit testing for hospital website')
+    
+    parser.add_argument("--test",action="store_true",help="Include to run tests")
+    parser.add_argument("--dryrun",action="store_true",help="Include to perform dry run of tests (but do not actually run them)")
+
+    args = parser.parse_args()
+
+    return args
+
+# Here are all my tests! - actual code located in send.py
+class Testing(unittest.TestCase):
+    # test some stuff
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    # test some other stuff
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+    
+    # test if you can connect to website
+    def test_connectivity(self):
+        connected = send.test_connection()
+        self.assertEqual(connected, 200)
+
+    # test for login to fail
+    def test_loginfailure(self):
+        response = send.test_failure()
+        self.assertEqual(response, 301)
+    
+    def test_loginpass(self):
+        response = send.test_login()
+        self.assertEqual(response, 201)
+
+if __name__ == '__main__':
+    #args = parse_args()
+
+    if len(sys.argv) > 1:
+        unittest.mock()
+    else:
+        unittest.main()
---
+++ send.py
@@ -0,0 +1,62 @@
+import http.client
+import json
+
+#template creds - these won't work though!
+username = "myuser"
+password = "mypassword"
+
+def test_connection():
+    connection = http.client.HTTPSConnection('zhwwmaomc.net', timeout=2)
+    print(connection)
+
+    connection.request("GET", "/")
+    response = connection.getresponse()
+    print("Status: {} and reason: {}".format(response.status, response.reason))
+
+    connection.close()
+
+    return response.status
+
+# this login should not work
+def test_failure():
+    connection = http.client.HTTPSConnection('zhwwmaomc.net', timeout=2)
+    print(connection)
+    headers = {'Content-type': 'application/json'}
+
+    message = {'username': username, 'password': password}
+    json_data = json.dumps(message)
+
+    connection.request('POST', '/post', json_data, headers)
+
+    response = connection.getresponse()
+    print(response.read().decode())
+
+    connection.close()
+
+    return response.status
+
+# this login should work
+def test_login():
+    connection = http.client.HTTPSConnection('zhwwmaomc.net', timeout=2)
+    print(connection)
+    headers = {'Content-type': 'application/json'}
+
+    creds = get_credentials()
+
+    message = {'username': creds[0], 'password': creds[1]}
+    json_data = json.dumps(message)
+
+    connection.request('POST', '/post', json_data, headers)
+
+    response = connection.getresponse()
+    print(response.read().decode())
+
+    connection.close()
+
+    return response.status
+
+# calculate credentials for login that will actually work
+def get_credentials():
+    un = "test" + "user" + str(50)
+    pw = "Password" + "1" + "!"
+    return [un, pw]
